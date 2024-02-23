2ea4d7b06767e97c5ac071f674d07a8244c01da0
Tiago <tiagonunesyou18@gmail.com>
2024-02-23 06:38:34 -0300

    Endereço finish

--- busca.py
+++ busca.py
@@ -4,16 +4,35 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
+import openpyxl
 
     
 service = Service()
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 
-url = "https://www.dfimoveis.com.br/aluguel/df/brasilia/apartamento"
+url = "https://www.dfimoveis.com.br/aluguel/df/brasilia/apartamento?&ordenamento=mais-recente"
 driver.get(url)
 
-teste = driver.find_elements(By.CSS_SELECTOR, 'option.ordenamento[value="mais-recente"]')
+arquivoExcel = openpyxl.load_workbook('Apartamentos.xlsx')
+planilha = arquivoExcel['Apartamentos']
+
+tamanhoDoResultadoDeBusca = len(driver.find_element(By.ID, 'resultadoDaBuscaDeImoveis').find_elements(By.TAG_NAME, 'a'))
+
+for apartamento in range(tamanhoDoResultadoDeBusca):
+    
+    linhaComeco = planilha.max_row + 1
+    planilha.cell(row=linhaComeco, column=1).value = apartamento + 1
+
+    endereço = driver.find_elements(By.TAG_NAME, 'h2')[apartamento].text
+    planilha.cell(row=linhaComeco, column=2).value = endereço
+
+
+    arquivoExcel.save('Apartamentos.xlsx')
+
+valor = ''
+valor_por_m2 = ''
+area = ''
 
 while True: time.sleep(1000)
 
