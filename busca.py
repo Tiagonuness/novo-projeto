513f0ad1d414e1874a5899eadc0f59809c16e220
Tiago <tiagonunesyou18@gmail.com>
2024-02-25 03:55:20 -0300

    Busca DF Imoveis incompleta

--- busca.py
+++ busca.py
@@ -3,13 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
-import time
 import openpyxl
-from openpyxl.styles import NamedStyle
 
-service = Service()
-options = webdriver.ChromeOptions()
-driver = webdriver.Chrome(service=service, options=options)
+driver = webdriver.Chrome()
 
 url = "https://www.dfimoveis.com.br/aluguel/df/brasilia/apartamento?&ordenamento=mais-recente"
 driver.get(url)
@@ -22,8 +18,7 @@ tamanhoDoResultadoDeBusca = len(driver.find_element(By.ID, 'resultadoDaBuscaDeIm
 for apartamento in range(tamanhoDoResultadoDeBusca):
     
     #Adcionar a ordem do imóvel no arquivo Excel
-    começo = 1
-    linhaComeco = começo + 1
+    linhaComeco = (1 + apartamento) + 1
     planilha.cell(row=linhaComeco, column=1).value = apartamento + 1
 
     #Coletar e adcionar o endereço no arquivo Excel
@@ -46,21 +41,14 @@ for apartamento in range(tamanhoDoResultadoDeBusca):
     imovel = driver.find_element(By.ID, 'resultadoDaBuscaDeImoveis').find_elements(By.TAG_NAME, 'a')[apartamento].find_elements(By.CLASS_NAME, 'new-details-ul')[0].text
     detalhes = imovel.split('\n')
     for li in range(len(detalhes)):
-        if 'm²' in detalhes[li]:
-            planilha.cell(row=linhaComeco, column=5).value = detalhes[li]
-        elif 'Quartos' in detalhes[li] or 'Quarto' in detalhes[li]:
-            planilha.cell(row=linhaComeco, column=6).value = detalhes[li]
-        elif 'Suítes' in detalhes[li] or 'Suíte' in detalhes[li]:
-            planilha.cell(row=linhaComeco, column=7).value = detalhes[li]
-        elif 'Vagas' in detalhes[li] or 'Vaga' in detalhes[li]:
-            planilha.cell(row=linhaComeco, column=8).value = detalhes[li]
+        if 'm²' in detalhes[li]: planilha.cell(row=linhaComeco, column=5).value = detalhes[li]
+        elif 'Quartos' in detalhes[li] or 'Quarto' in detalhes[li]: planilha.cell(row=linhaComeco, column=6).value = detalhes[li]
+        elif 'Suítes' in detalhes[li] or 'Suíte' in detalhes[li]: planilha.cell(row=linhaComeco, column=7).value = detalhes[li]
+        elif 'Vagas' in detalhes[li] or 'Vaga' in detalhes[li]: planilha.cell(row=linhaComeco, column=8).value = detalhes[li]
 
-
-
+    #Obter link do imóvel e adcionar na planilha Excel
+    imoveLink = driver.find_element(By.ID, 'resultadoDaBuscaDeImoveis').find_elements(By.TAG_NAME, 'a')[apartamento].get_attribute('href')
+    planilha.cell(row=linhaComeco, column=9).value = imoveLink
 
     #Salvar todas alterações 
-    começo += 1
     arquivoExcel.save('Apartamentos.xlsx')
-
-while True: time.sleep(1000)
-
